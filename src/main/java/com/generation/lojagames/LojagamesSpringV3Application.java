970c5d30468b1ca2a9e5261aa47cebd850650fa7
Fernando Sartori <fehsartorisos@gmail.com>
2023-05-22 12:02:11 -0300

    primeiro commit

---
+++ src/main/java/com/generation/lojagames/LojagamesSpringV3Application.java
@@ -0,0 +1,13 @@
+package com.generation.lojagames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojagamesSpringV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojagamesSpringV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/lojagames/LojagamesSpringV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.lojagames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojagamesSpringV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
